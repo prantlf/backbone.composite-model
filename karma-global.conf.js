ec79b1c5506b414c70fb10c27412cd2d4db3a249
Ferdinand Prantl <prantlf@gmail.com>
2018-03-17 02:13:25 +0100

    chore: Upgrade semantic release and qunit

--- karma-global.conf.js
+++ karma-global.conf.js
@@ -14,8 +14,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/qunitjs/qunit/qunit.css',
-      'node_modules/qunitjs/qunit/qunit.js',
+      'node_modules/qunit/qunit/qunit.css',
       'node_modules/jquery/dist/jquery.js',
       'node_modules/underscore/underscore.js',
       'node_modules/backbone/backbone.js',
